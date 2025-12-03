4c5ebe60e853c2990411f1efa97509fed901e20e
ArianaCo02 <144282253+ArianaCo02@users.noreply.github.com>
2025-12-02 22:53:14 -0500

    Update Creamer.vue

--- src/components/Creamer.vue
+++ src/components/Creamer.vue
@@ -1,30 +1,45 @@
 <template>
-  <div class="froth">
-    <div
-      v-for=" in 5"
-      class="foam"
-      :style="{ backgroundColor: beverageStore.currentCreamer?.color }"
-    ></div>
+  <div class="froth" :style="{ backgroundColor: color }">
+    <!-- creates 5 foam bubbles -->
+    <div v-for="n in 5" :key="n" class="foam"></div>
   </div>
 </template>
 
 <script setup lang="ts">
-import { useBeverageStore } from "../stores/beverageStore";
+import { computed } from "vue"
 
-const beverageStore = useBeverageStore();
+// get the creamer choice from Beverage.vue
+const props = defineProps({
+  type: String
+})
+
+// change the color of the froth depending on what the user picked
+const color = computed(() => {
+  if (props.type === "Milk") {
+    return "#fffbe9"    // light cream color
+  } else if (props.type === "Cream") {
+    return "#fff4d6"    // slightly darker
+  } else if (props.type === "Half & Half") {
+    return "#f0e6d2"    // beige
+  } else {
+    return "#c6c6c6"    // default color
+  }
+})
 </script>
-<style lang="scss" scoped>
+
+<style scoped lang="scss">
 .froth {
-  overflow: visible;
-  transform: translateY(400%);
   position: relative;
   height: 20%;
   width: 100%;
+  overflow: visible;
+  transform: translateY(400%);
   background-color: #c6c6c6;
   animation: pour-tea 2s 2s forwards;
+  transition: background-color 0.3s;
 }
+
 .foam {
-  display: block;
   background: #e4e0d2;
   border-radius: 30px;
   height: 40px;
@@ -32,34 +47,10 @@ const beverageStore = useBeverageStore();
   position: absolute;
 }
 
-.foam:nth-child(1) {
-  top: 0px;
-  left: -3px;
-}
 
-.foam:nth-child(2) {
-  top: 0px;
-  left: 55px;
-}
-
-.foam:nth-child(3) {
-  width: 30px;
-  height: 30px;
-  border-radius: 40px;
-  top: 3px;
-  left: 30px;
-}
-
-.foam:nth-child(4) {
-  width: 30px;
-  height: 30px;
-  border-radius: 45px;
-  top: 5px;
-  right: -2px;
-}
-
-.foam:nth-child(5) {
-  top: 2px;
-  right: 10px;
-}
+.foam:nth-child(1) { top: 0; left: -3px; }
+.foam:nth-child(2) { top: 0; left: 55px; }
+.foam:nth-child(3) { top: 3px; left: 30px; width: 30px; height: 30px; border-radius: 40px; }
+.foam:nth-child(4) { top: 5px; right: -2px; width: 30px; height: 30px; border-radius: 45px; }
+.foam:nth-child(5) { top: 2px; right: 10px; }
 </style>
